3d7ac4baf4f64d0651340af0a9d33ae80ad04494
Ziyang Li <ziyang.li.nk@gmail.com>
2016-07-18 11:33:49 +0200

    no message

---
+++ build.sh
@@ -0,0 +1,5 @@
+pdflatex TeX/TUDthesis.tex
+bibtex TeX/TUDthesis >> log.txt
+pdflatex TeX/TUDthesis.tex
+pdflatex TeX/TUDthesis.tex
+pdflatex TeX/TUDthesis.tex
--- buildpdf.sh
+++ buildpdf.sh
@@ -1,3 +1,3 @@
 #vagrant up
-ssh vagrant@127.0.0.1 -p 2222 -i ./.vagrant/machines/default/virtualbox/private_key "cd /vagrant; sh build.sh;"
+ssh vagrant@127.0.0.1 -p 2222 -i ./.vagrant/machines/default/virtualbox/private_key "cd /TeX; sh build.sh;"
 #open index.pdf
